2648794edd2dac7bc5a3485d806ea28dcfd67ded
nelsonbolivar <koombea@MacBook-Pro-de-koombea-2.local>
2016-03-17 12:06:31 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 32adef649cd4e7632dada433d04a8ef87a460885, reversing
    changes made to 24e2af044365410ec785196941afba1a99799a3c.

--- iOSPush/iOSPush-Bridging-Header.h
+++ iOSPush/iOSPush-Bridging-Header.h
@@ -2,10 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-<<<<<<< HEAD
 #import <OneSignal/OneSignal.h>
-=======
-#import "Mixpanel/Mixpanel.h"
-#import <OneSignal/OneSignal.h>
-#import <BuddyBuildSDK/BuddyBuildSDK.h>
->>>>>>> origin/master
